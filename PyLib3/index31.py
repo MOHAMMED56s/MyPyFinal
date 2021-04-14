818e47662850679d11afe8d9e4531ac5e9b2b547
MOHAMMED56s <81165865+MOHAMMED56s@users.noreply.github.com>
2021-04-14 13:14:49 +0400

    Add files via upload

---
+++ PyLib3/index31.py
@@ -0,0 +1,9 @@
+#program to print the grater number between two numbers
+a=7
+b=9
+if(a > b):
+ print("a is greater than b")
+elif(b > a):
+ print("b is greater than a")
+else:
+ print("b is eqal to a")
